49480be2eae64b511457e25de568b4eee4ac5129
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-01-30 23:57:08 -0800

    finish explanations for hello world express.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+# the constant express imports express module using the keyword 'require'
 const express = require('express')
+# see https://stackoverflow.com/questions/27599614/var-express-requireexpress-var-app-express-what-is-express-is-it for commentary on what
+# express() is, basically a function
 const app = express()
+# this constant is just defining the number of the port that we'll be connected to later
 const port = 3000
 
+# this is the first route, allowing the app to request and response, in which the response is "Hello World!"
 app.get('/', (req, res) => res.send('Hello World!'))
 
+# this prints in the console
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
