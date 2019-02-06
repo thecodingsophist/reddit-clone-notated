9777755878256f81fcc6f5cbe82c835bbc0b1ebb
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-02-05 23:25:04 -0800

    bootstrap works

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-# the constant express imports express module using the keyword 'require'
+// the constant express imports express module using the keyword 'require'
 const express = require('express')
-# see https://stackoverflow.com/questions/27599614/var-express-requireexpress-var-app-express-what-is-express-is-it for commentary on what
-# express() is, basically a function
+// see https://stackoverflow.com/questions/27599614/var-express-requireexpress-var-app-express-what-is-express-is-it for commentary on what
+// express() is, basically a function
 const app = express()
-# this constant is just defining the number of the port that we'll be connected to later
+// this constant is just defining the number of the port that we'll be connected to later
 const port = 3000
+const path = require('path')
 
-# this is the first route, allowing the app to request and response, in which the response is "Hello World!"
-app.get('/', (req, res) => res.send('Hello World!'))
+// this is the first route, allowing the app to request and response, in which the response is "Hello World!"
 
-# this prints in the console
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, 'front_page_of_the_internet.html'));
+});
+
+// this prints in the console
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
