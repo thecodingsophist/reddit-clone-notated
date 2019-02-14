465c49d8ef6094812fd14ba73873702408281c92
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-02-14 13:26:06 -0800

    got navbar working

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const app = express()
 // this constant is just defining the number of the port that we'll be connected to later
 const port = 3000
 const path = require('path')
+const exphbs = require('express-handlebars')
+//set up for handlebars, in order to use an npm package, you have to import it and then 'use' it to run
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // this is the first route, allowing the app to request and response, in which the response is "Hello World!"
 
 app.get('/',function(req,res) {
-  res.sendFile(path.join(__dirname, 'main.html'));
+    //res.render
+  res.render('home');
 });
 
 // this prints in the console
